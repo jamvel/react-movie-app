9e6121abfa70cf808e9ed44c51595d16e21f86f7
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-22 20:13:01 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import Header from 'components/Header'
 import './App.css';
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Header />
     </div>
   );
 }
