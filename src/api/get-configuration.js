42db2cfe0e539e648ceda993242ad904c0c9f782
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-23 21:34:59 +0200

    Fix indentation

--- src/api/get-configuration.js
+++ src/api/get-configuration.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default async () => {
     try {
-       const { data } = await axios.get('/configuration')
-       return data;
+        const { data } = await axios.get('/configuration')
+        return data;
     }catch(e){
         throw (e)
     }
