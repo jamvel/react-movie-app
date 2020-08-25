08e9123bbf73f070f640699495163793191aac6a
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-26 00:28:01 +0200

    Rename parameter in getList action

--- src/stores/movies/actions.js
+++ src/stores/movies/actions.js
@@ -21,13 +21,13 @@ export const setMovies = movies => dispatch => {
 }
 
 
-export const getList = ({ id, apiCallback } = {}) => async (dispatch, getState) => {
+export const getList = ({ id, fetchDataCallback } = {}) => async (dispatch, getState) => {
     // PARAMS must be provided - throw error outside catch as an unrecoverable error
     if(id === undefined){
         throw new Error('Missing List ID in getList Action')
     }
 
-    if(apiCallback === undefined){
+    if(fetchDataCallback === undefined){
         throw new Error('Missing List callback in getList Action')
     }
 
@@ -43,7 +43,7 @@ export const getList = ({ id, apiCallback } = {}) => async (dispatch, getState) 
             }
         })
 
-        const data = await apiCallback({ pageIndex: currentPageIndex + 1 })
+        const data = await fetchDataCallback({ pageIndex: currentPageIndex + 1 })
 
         const list = data.results.map(movie => movie.id)
 
@@ -74,8 +74,8 @@ export const getList = ({ id, apiCallback } = {}) => async (dispatch, getState) 
     }
 };
 
-export const getPopularList = () => getList({ id: 'popular', apiCallback: getPopular })
-export const getTopRatedList = () => getList({ id: 'topRated', apiCallback: getTopRated })
+export const getPopularList = () => getList({ id: 'popular', fetchDataCallback: getPopular })
+export const getTopRatedList = () => getList({ id: 'topRated', fetchDataCallback: getTopRated })
 
 // export const getMovieDetails = id => async dispatch => {
 //     try {
