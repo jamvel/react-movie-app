116d4a0c3ba566b9394a34c0ff7a1093bfcef3a0
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-26 00:48:43 +0200

    Fix indentation in MovieCard component

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -4,56 +4,57 @@ import get from 'lodash.get';
 import styled from 'styled-components';
 
 const MovieCardParent = styled.div`
-	position: relative;
-	flex: 0 0 17%;
-	display: flex;
-	justify-content: space-around;
-	margin: 1em;
+    position: relative;
+    flex: 0 0 17%;
+    display: flex;
+    justify-content: space-around;
+    margin: 1em;
     transition: transform 0.3s ease;
-	color: white;
+    color: white;
 
     :hover {
         cursor: pointer;
-		transform: scale(1.1);
-		z-index: 2;
-	}
-	@media screen and (max-width: 1440px) {
-		flex: 1 0 15%;
-	}
-	@media screen and (max-width: 720px) {
-		flex: 0 0 25%;
-	}
-	@media screen and (max-width: 540px) {
-		flex: 1 0 33%;
-	}
+        transform: scale(1.1);
+        z-index: 2;
+    }
+
+    @media screen and (max-width: 1440px) {
+        flex: 1 0 15%;
+    }
+    @media screen and (max-width: 720px) {
+        flex: 0 0 25%;
+    }
+    @media screen and (max-width: 540px) {
+        flex: 1 0 33%;
+    }
 `
 
 const StyledImg = styled.img`
-	width: 100%;
-	height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 const MovieCard = ({ movie, secureBaseUrl, posterSize }) => (
-	<>
-		{/* check that movie is defined in state before rendering */}
-		{movie && (
-			<MovieCardParent onClick={() => console.log(get(movie, 'id'))}>
-			<StyledImg
-				src={`${secureBaseUrl}${posterSize}/${get(movie, 'poster_path')}`}
-			/>
-			</MovieCardParent>
-		)}
-	</>
+    <>
+        {/* check that movie is defined in state before rendering */}
+        {movie && (
+            <MovieCardParent onClick={() => console.log(get(movie, 'id'))}>
+            <StyledImg
+                src={`${secureBaseUrl}${posterSize}/${get(movie, 'poster_path')}`}
+            />
+            </MovieCardParent>
+        )}
+    </>
 )
 
 const mapStateToProps = (state, props) => {
-	const data = get(state, 'movies.data');
-	const imagesConfig = get(state, 'config.images');
-	
+    const data = get(state, 'movies.data');
+    const imagesConfig = get(state, 'config.images');
+    
     return {
-		movie: data[props.id] || null,
-		secureBaseUrl: get(imagesConfig, 'secure_base_url'),
-		posterSize: get(imagesConfig, 'poster_sizes[3]')
+        movie: data[props.id] || null,
+        secureBaseUrl: get(imagesConfig, 'secure_base_url'),
+        posterSize: get(imagesConfig, 'poster_sizes[3]')
     }
 }
 
