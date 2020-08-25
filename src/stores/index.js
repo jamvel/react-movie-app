0a88f4605bfd29f14d1bde45a3703ed1d8486f9c
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-25 18:33:31 +0200

    Fix bug causing crash on Safari due to chrome extension and add app reducer

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,15 +2,17 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import config from './config/reducer';
 import movies from './movies/reducer';
+import app from './app/reducer';
 
 const makeStore = () => createStore(
     combineReducers({
         config,
-        movies
+        movies,
+        app
     }),
     compose(
         applyMiddleware(thunk),
-        typeof window !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f, // eslint-disable-line no-underscore-dangle
+        typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f, // eslint-disable-line no-underscore-dangle
     ),
 );
 
