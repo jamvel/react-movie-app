18f70957c5c38ddfe0806f8ee38c1aa799d0b42d
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-25 15:06:34 +0200

    Update Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default () => (
-    <div>Header</div>
+import Search from 'components/Search';
+
+const HeaderWrapper = styled.div`
+    width: 100%;
+    height: auto;
+    color: white;
+    font-size: 1.5em;
+`
+
+const BrandWrapper = styled.div`
+    margin: 0 10px;
+`
+
+const Header = () => (
+    <HeaderWrapper>
+        <BrandWrapper>
+            <FontAwesomeIcon icon="film" /> React <span>Movies</span>
+        </BrandWrapper>
+        <Search />
+    </HeaderWrapper>
 )
+
+export default Header;
