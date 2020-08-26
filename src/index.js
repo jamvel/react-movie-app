88f9b5f38afcbc6374a73a89b105ef92ae9af932
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-26 11:48:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import App from 'components/App';
+import makeStore from './stores'
 import * as serviceWorker from './serviceWorker';
 
+axios.defaults.baseURL = process.env.REACT_APP_TMDB_API_BASE_URL;
+
+// Check if key is defined, if not assume a proxy is being used
+if(process.env.REACT_APP_TMDB_API_KEY){
+    axios.defaults.params = {}
+    axios.defaults.params['api_key'] = process.env.REACT_APP_TMDB_API_KEY;
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={makeStore()}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
