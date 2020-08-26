d7de24a9f5bcf39aae1bae37396c1a42b2d62f0b
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-26 11:49:17 +0200

    Update getMovieDetails api call

--- src/api/get-movie-details.js
+++ src/api/get-movie-details.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
-export default async id => {
+const defaultParams = {
+    appendToResponse: 'videos,similar'
+}
+
+export default async (id, { appendToResponse } = defaultParams) => {
     try {
         if(!id){
             throw new Error('Movie ID cannot be undefined')
         }
-        const { data } = await axios.get(`/movie/${id}`)
+        const { data } = await axios.get(`/movie/${id}`, {
+            params: {
+                append_to_response: appendToResponse
+            }
+        })
         return data;
     }catch(e){
         throw (e)
