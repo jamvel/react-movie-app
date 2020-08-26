e3ec341d223776a20d65d25adda6dd3994e376a7
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-26 14:39:56 +0200

    Added link to Brand in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useHistory } from 'react-router-dom';
 
 import { toggleSearch } from 'stores/app/actions'
 
@@ -17,6 +18,7 @@ const HeaderParent = styled.div`
 const BrandWrapper = styled.div`
     margin: 0 20px;
     user-select: none;
+    cursor: pointer;
 `
 
 const HeaderWrapper = styled.div`
@@ -31,22 +33,25 @@ const HeaderWrapper = styled.div`
     }
 `
 
-const Header = ({ showSearch, toggleSearchRx }) => (
-    <HeaderParent>
-        <HeaderWrapper>
-            <BrandWrapper>
-                <FontAwesomeIcon icon="film" /> React <span>Movies</span>
-            </BrandWrapper>
-            <FontAwesomeIcon 
-                icon={showSearch ? 'times' : 'search'}
-                onClick={() => toggleSearchRx()}
-            />
-        </HeaderWrapper>
-        {showSearch && (
-            <Search />
-        )}
-    </HeaderParent>
-)
+const Header = ({ showSearch, toggleSearchRx }) => {
+    const history = useHistory();
+    return (
+        <HeaderParent>
+            <HeaderWrapper>
+                <BrandWrapper onClick={() => history.push('/')}>
+                    <FontAwesomeIcon icon="film" /> React <span>Movies</span>
+                </BrandWrapper>
+                <FontAwesomeIcon 
+                    icon={showSearch ? 'times' : 'search'}
+                    onClick={() => toggleSearchRx()}
+                />
+            </HeaderWrapper>
+            {showSearch && (
+                <Search />
+            )}
+        </HeaderParent>
+    )
+}
 
 const mapStateToProps = state => {
     return {
