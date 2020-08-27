97e07ab20b5b027ce592b9781faee5276254f525
Jamie Vella <jamie.vella@raketech.com>
2020-08-27 11:49:15 +0200

    Updates

---
+++ precache-manifest.9c1a14d5c6cdfae70fece238aa09c8f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04e320caaedf1dd649b01b648237126",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "40e382b9efdf44fd08d7",
+    "url": "/react-movie-app/static/js/2.7381d7f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.7381d7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78678fa2034c772226c3",
+    "url": "/react-movie-app/static/js/main.01571432.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
