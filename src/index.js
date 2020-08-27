6d1e3ad2f995caaaa95cf4d6bd72d90b44ce37a5
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-08-27 12:21:27 +0200

    Change Router to HashRouter to fixing routing bug in gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from 'components/App';
 import makeStore from './stores'
 import * as serviceWorker from './serviceWorker';
@@ -18,9 +18,9 @@ if(process.env.REACT_APP_TMDB_API_KEY){
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={makeStore()}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
