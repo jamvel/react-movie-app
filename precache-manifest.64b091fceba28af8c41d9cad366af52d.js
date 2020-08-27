acef274b87a02c75186e3147f1353f907aba30c5
Jamie Vella <jamie.vella@raketech.com>
2020-08-27 12:17:39 +0200

    Updates

---
+++ precache-manifest.64b091fceba28af8c41d9cad366af52d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6bbca572068926a04218c97497ad98",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "30219388545c5cc901e3",
+    "url": "/react-movie-app/static/js/2.83fd553e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.83fd553e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be563a95635e2e4c672",
+    "url": "/react-movie-app/static/js/main.879197af.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-movie-app/precache-manifest.5dd7f9cfc8cf5de4f804b9cc836fcff4.js"
+  "/react-movie-app/precache-manifest.64b091fceba28af8c41d9cad366af52d.js"
 );
 
 self.addEventListener('message', (event) => {
